6c1c34dda28033be67b54abaa947e6ed3efa2f08
Colter Garrison <102547626+Colter-Garrison@users.noreply.github.com>
2022-06-27 10:53:34 -0700

    things showing on page and clickable

--- src/App.js
+++ src/App.js
@@ -1,11 +1,71 @@
+import { useState } from 'react';
+import { 
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from 'react-router-dom';
+import AuthPage from './AuthPage';
+import CreatePage from './CreatePage';
+import ListPage from './ListPage';
+import UpdatePage from './UpdatePage';
+import { client } from './services/client';
+import { logout } from './services/fetch-utils';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+export default function App() {
+  const [user, setUser] = useState(client.auth.user());
 
-    </div>
+  async function handleLogoutClick() {
+    await logout();
+    setUser('');
+  }
+
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/'>Sign In</Link>
+            </li>
+            <li>
+              <Link to='/create'>Create new Beer</Link>
+            </li>
+            <li>
+              <Link to='/beers/1'>Update a Beer</Link>
+            </li>
+            <li>
+              <Link to='/beers'>List of Your Beers</Link>
+            </li>
+            <li>
+              {user &&
+              <button onClick={handleLogoutClick}>LogOut</button>}
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route exact path='/'>
+            {
+              !user ? <AuthPage setUser={setUser} />
+                : <Redirect to='/beers' />
+            }
+          </Route>
+          <Route exact path='/beers/:id'>
+            <UpdatePage />
+          </Route>
+          <Route exact path='/beers'>
+            {
+              user ? <ListPage />
+                : <Redirect to='/' />
+            }
+          </Route>
+          <Route exact path='/create'>
+            <CreatePage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;
--- src/AuthPage.js
+++ src/AuthPage.js
@@ -1,2 +1,59 @@
 import { useState } from 'react';
 import { signIn, signUp } from './services/fetch-utils';
+
+export default function AuthPage({ setUser }) {
+  const [error, setError] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [signInEmail, setSignInEmail] = useState('');
+  const [signInPassword, setSignInPassword] = useState('');
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    try {
+      const user = await signUp(email, password);
+      setUser(user);
+    } catch (e) {
+      setError(e.message);
+    }
+  }
+
+  async function handleSignInSubmit(e) {
+    e.preventDefault();
+    const user = await signIn(signInEmail, signInPassword);
+
+    setUser(user);
+  }
+
+  return (
+    <div>
+      <h3>BEER!</h3>
+      <h1 className='error'>{error}</h1>
+      <form onSubmit={handleSubmit}>
+        <p>Sign Up</p>
+        <label>
+          email
+          <input onChange={e => setEmail(e.target.value)} value={email} type='email' />
+        </label>
+        <label>
+          password
+          <input onChange={e => setPassword(e.target.value)} value={password} type='password' />
+        </label>
+        <button>Sign Up</button>
+      </form>
+      <form onSubmit={handleSignInSubmit}>
+        <p>Sign In</p>
+        <label>
+          email
+          <input onChange={e => setSignInEmail(e.target.value)} value={signInEmail} type='email' />
+        </label>
+        <label>
+          password
+          <input onChange={e => setSignInPassword(e.target.value)} value={signInPassword} type='password' />
+        </label>
+        <button>Sign In</button>
+      </form>
+    </div>
+  );
+}
